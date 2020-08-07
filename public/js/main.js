b16144bce815c95b88853fc2615748effe60fc39
Egor Babintsev <e.e.babintsev@yandex.ru>
2020-08-07 15:45:52 +0300

    Add all neccesary styles for responsive navigation menu, add body lock class add modified menu and footer to index and brands

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 document.querySelector('.navi-toggler svg').addEventListener('click', function() {
   this.classList.toggle('active');
   document.querySelector('.navi-menu').classList.toggle('hidden');
+  document.querySelector('body').classList.toggle('is-locked');
 })
