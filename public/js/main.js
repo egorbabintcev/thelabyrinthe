c24e16234eb9b6326d7c544dd6060c2cdd57ff5b
Egor Babintsev <e.e.babintsev@yandex.ru>
2020-08-07 10:08:54 +0300

    Add hamburger mobile menu

---
+++ public/js/main.js
@@ -0,0 +1,4 @@
+document.querySelector('.navi-toggler svg').addEventListener('click', function() {
+  this.classList.toggle('active');
+  document.querySelector('.navi-menu').classList.toggle('hidden');
+})
