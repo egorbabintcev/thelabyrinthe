d23e8f7a37bbcedc45aa3629eabfbd69a4f1807a
Egor Babintsev <e.e.babintsev@yandex.ru>
2020-08-11 15:02:33 +0300

    Create slider with management personal

--- public/js/main.js
+++ public/js/main.js
@@ -53,3 +53,43 @@ $('form input[type="checkbox"]').on('change', function() {
     submitBtn.prop('disabled', true);
   }
 })
+
+$('button.slide-investor__control-btn').on('click', function(e) {
+  e.preventDefault();
+  const curSlide = $(this).closest('.slide');
+  const menu = $(this).closest('.management-slider').find('.menu ul li');
+  menu.filter('.is-active').removeClass('is-active');
+  if ($(this).hasClass('prev')) {
+
+    let prevSlide;
+    if (curSlide.prev().length !== 0) {
+      prevSlide = curSlide.prev();
+    } else {
+      prevSlide = curSlide.siblings().last();
+    }
+
+    prevSlide.addClass('is-active');
+    $(menu[prevSlide.index()]).addClass('is-active');
+  } else if ($(this).hasClass('next')) {
+
+    let nextSlide;
+    if (curSlide.next().length !== 0) {
+      nextSlide = curSlide.next();
+    } else {
+      nextSlide = curSlide.siblings().first();
+    }
+
+    nextSlide.addClass('is-active');
+    $(menu[nextSlide.index()]).addClass('is-active');
+  }
+  curSlide.removeClass('is-active');
+})
+
+$('.management-slider li').on('click', function() {
+  $(this).siblings('.is-active').removeClass('is-active');
+  $(this).addClass('is-active');
+
+  const slides = $(this).closest('.management-slider').find('.slide');
+  slides.filter('.is-active').removeClass('is-active');
+  $(slides[$(this).index()]).addClass('is-active');
+})
